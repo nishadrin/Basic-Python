42b312c360ecef500b0d0fe16a9a22108b776010
nishadrin <nishadrin@gmail.com>
2018-12-10 01:21:37 +0300

    hw04 easy and normal 2/3

--- lesson-4/hw04_easy.py
+++ lesson-4/hw04_easy.py
@@ -6,9 +6,32 @@
 # квадратами элементов исходного списка
 # [1, 2, 4, 0] --> [1, 4, 16, 0]
 
+import random
+
+before = int(input("Мин. рандомное значение (от): "))
+after = int(input("Макс. рандомное значение (до): "))
+range_ = int(input("Введите кол-во цифр в списке: "))
+
+lst_gen = [random.randint(before, after) for _ in range(range_)]
+
+lst_gen = [1, 2, 4, 0]
+
+def square_list(list):
+    square = [i**2 for i in list]
+    return square
+
+print(square_list(lst_gen))
+
 # Задание-2:
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
+
+fruit_list1 = ["apple", "orange", "banana", "pineapple", "kiwi"]
+fruit_list2 = ["orange", "apple", "banana", "kiwi"]
+
+fruit_list3 = [i for i in fruit_list1 for j in fruit_list2 if j == i]
+
+print(fruit_list3)
 
 # Задание-3:
 # Дан список, заполненный произвольными числами.
@@ -16,3 +39,17 @@
 # + Элемент кратен 3
 # + Элемент положительный
 # + Элемент не кратен 4
+
+import random
+
+before = int(input("Мин. рандомное значение (от): "))
+after = int(input("Макс. рандомное значение (до): "))
+range_ = int(input("Введите кол-во цифр в списке: "))
+
+lst_gen = [random.randint(before, after) for _ in range(range_)]
+
+lst_gen = [1, 2, 4, 6, -25, 12]
+
+newlist = [i for i in lst_gen if i > 0 and i % 3 == 0 and i % 4 != 0]
+
+print(newlist)
